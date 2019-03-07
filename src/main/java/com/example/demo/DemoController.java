b5771cca24038726caad3acb5b879ce35a182048
git <rporzio@sorint.it>
2019-03-07 10:32:53 +0100

    test for OpenShift

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -10,6 +10,7 @@ import java.sql.Timestamp;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Stream;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,54 +18,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
 
-	@RequestMapping("/demo")
-	public String demo(@RequestParam(value = "name", defaultValue = "World") String name) {
+	@RequestMapping("/")
+	public String welcome() {
 		
-		readFile();
-		return name;
+
+		return "Welcome to Demo";
 	}
 
-	public void readFile() {
-		String fileInput = "C:\\Users\\CommVault\\Documents\\workspace-sts-3.9.6.RELEASE\\demo\\input.txt";
-		String fileOutput = "C:\\Users\\CommVault\\Documents\\workspace-sts-3.9.6.RELEASE\\demo\\output.txt";
-		// read file into stream, try-with-resources
-
-		try {
-			final BufferedWriter writer = new BufferedWriter(new FileWriter(fileOutput,true));
-
-			BufferedReader br = Files.newBufferedReader(Paths.get(fileInput));
-
-			Stream<String> lines = br.lines().map(str -> getOddEven(new Integer(str)));
-
-			lines.forEach(arg0 -> {
-				try {
-
-					java.util.Date date = new java.util.Date();
-					Timestamp t = new Timestamp(date.getTime());
-					System.out.println(t + ":" + arg0 + "\n");
-					writer.write(t + ":" + arg0 + "\n");
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			});
-			lines.close();
-			writer.close();
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	@RequestMapping("/{input}")
+	public String congrats(@PathVariable String input) {
+		return "Hi " + input + " ypur application deployed successfully "	;
 	}
 
-	public String getOddEven(Integer value) {
-		if (value % 2 == 0) {
-			return "EVEN";
-		} else {
-			return "ODD";
-		}
-
-	}
-
-	
 
 }
